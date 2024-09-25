6ac7fe6dbd53657ebe2d781888f95d83601faed7
morkuntsov <morkuntsov@yandex-team.ru>
2024-09-25 20:15:41 +0300

    dfs

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 
-set GITSYNC_WORKDIR=C:\gitsync\git_local\src\cf\
-set GITSYNC_STORAGE_PATH=C:\gitsync\master
+set GITSYNC_WORKDIR=C:/gitsync/git_local/src/cf/
+set GITSYNC_STORAGE_PATH=C:/gitsync/master
 set GITSYNC_STORAGE_USER=deploy
 set GITSYNC_STORAGE_PWD=
 set GITSYNC_REMOTE_PUSH=true
