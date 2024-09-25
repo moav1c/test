a31936136835ae9e261bde34bfa755f9c06254fd
morkuntsov <morkuntsov@yandex-team.ru>
2024-09-25 20:18:06 +0300

    sad

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 
-set GITSYNC_WORKDIR=C:/gitsync/git_local/src/cf/
-set GITSYNC_STORAGE_PATH=C:/gitsync/master
-set GITSYNC_STORAGE_USER=deploy
+set GITSYNC_WORKDIR='C:/gitsync/git_local/src/cf/'
+set GITSYNC_STORAGE_PATH='C:/gitsync/master'
+set GITSYNC_STORAGE_USER='deploy'
 set GITSYNC_STORAGE_PWD=
 set GITSYNC_REMOTE_PUSH=true
 set GITSYNC_REMOTE_PULL=true
