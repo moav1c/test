c6e1f76114278dc6ee94d31dc00eac9f6ee410e7
morkuntsov <morkuntsov@yandex-team.ru>
2024-09-27 09:09:55 +0300

    sa

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,9 @@
 
-set GITSYNC_WORKDIR='C:/gitsync/git_local/src/cf/'
-set GITSYNC_STORAGE_PATH='C:/gitsync/master'
-set GITSYNC_STORAGE_USER='deploy'
-set GITSYNC_STORAGE_PWD=
-set GITSYNC_REMOTE_PUSH=true
-set GITSYNC_REMOTE_PULL=true
-gitsync -v sync 
+dev GITSYNC_WORKDIR='C:/gitsync/git_local/src/cf/'
+dev GITSYNC_STORAGE_PATH='C:/gitsync/master'
+dev GITSYNC_REPO_URL='https://github.com/moav1c/test/tree/main'
+dev GITSYNC_STORAGE_USER='deploy'
+dev GITSYNC_STORAGE_PWD=
+dev GITSYNC_REMOTE_PUSH=true
+dev GITSYNC_REMOTE_PULL=true
+gitsync sync -u $GITSYNC_STORAGE_USER -P -G $GITSYNC_STORAGE_PATH $GITSYNC_WORKDIR $GITSYNC_REPO_URL
